f4cc553f34b1b644fbf2ef9e766e54124f8df9fc
feiyupianshui <ed273030@gmail.com>
2017-09-20 17:43:33 +0800

    0920

--- radiowave/spiders/radiowave.py
+++ radiowave/spiders/radiowave.py
@@ -1,20 +1,27 @@
 #!/use/bin/env python
 #_*_coding:utf-8_*_
 import scrapy
+import re
 from scrapy.spider import CrawlSpider, Rule, Request
 from scrapy.linkextractors import LinkExtractor
-from scrapy import FormRequest
+# from scrapy import FormRequest
+from bs4 import BeautifulSoup
 from radiowave.items import RadiowaveItem
 
 class myspider(CrawlSpider):
     name = 'radiowave'
     allowed_domain = ['dbfansub.com']
-    start_url = ['http://dbfansub.com/']
+    start_urls = ['http://dbfansub.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=('\.html')), callback = 'parse_item', follow=True),
-        # Rule(LinkExtractor(allow=('/categoty/[a-z]+/')))
+        Rule(LinkExtractor(allow=('\.html',), deny =('weibo','qq','redirect','login',)), callback = 'parse_item', follow=True),
+        #放行可用链接
     )
-    def parse_item(self,reponse):
-        print(response.url)
-        pass
+    def parse_item(self,response):
+        # print(response.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pans = soup.find_all('a', href=re.compile("baidu"))
+        for a in pans:
+            panurl = a['href']
+# 提取名字和集数的链接：
+# <h1 class="entry-title h3" itemprop="name">【动画】瑞克与莫蒂 Rick and Morty 第三季(更新至08集) 电波字幕组</h1>
